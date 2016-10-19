8c412388b6862c1cbae8f63bff6df4d7bf78440a
BA201 <BA201@MCBA201-C33.classrooms.stlcc>
2016-10-19 10:17:30 -0500

    First commit

---
+++ src/gethub/GetHub.java
@@ -0,0 +1,16 @@
+/*Dallas Wyciskalla
+ * 10/19/16
+ * 
+ */
+package gethub;
+
+public class GetHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
